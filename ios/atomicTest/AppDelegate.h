6d0995d3b2378c742739c66eb2000d11b11d69e0
Darlison Alves <darlinsonalves@gmail.com>
2020-10-18 21:59:18 -0300

    configurando host para redirecionamento para o app...

--- ios/atomicTest/AppDelegate.h
+++ ios/atomicTest/AppDelegate.h
@@ -1,8 +1,17 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
+#import <React/RCTLinkingManager.h>
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+  return [RCTLinkingManager application:application openURL:url
+                      sourceApplication:sourceApplication annotation:annotation];
+}
 
 @end
